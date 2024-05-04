15b77855be4824a9051de45e766d1a82eecf2720
mydreams2018 <mydreams2018@outlook.com>
2024-05-04 17:47:16 +0800

    base 2

--- src/main/java/com/example/javafxdemo/ScrollPaneDemo.java
+++ src/main/java/com/example/javafxdemo/ScrollPaneDemo.java
@@ -1,8 +1,10 @@
 package com.example.javafxdemo;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -14,11 +16,17 @@ public class ScrollPaneDemo extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         ScrollPane root = new ScrollPane();
-        Rectangle rect = new Rectangle(300, 300, Color.RED);
-        root.setPrefSize(200, 200);
-        root.setContent(rect);
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
+        Rectangle rect = new Rectangle(800, 800, Color.RED);
+        hbox.getChildren().add(rect);
+        root.setContent(hbox);
+        root.setFitToWidth(true);
+        root.setFitToHeight(true);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(600);
         primaryStage.setTitle("TabPane Demo");
         primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.show();
