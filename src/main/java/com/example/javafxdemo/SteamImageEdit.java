365cea4cff0eff545ea7ccbbc00bbe5d1c8687dc
mydreams2018 <mydreams2018@outlook.com>
2025-01-09 21:38:02 +0800

    steam image

--- src/main/java/com/example/javafxdemo/SteamImageEdit.java
+++ src/main/java/com/example/javafxdemo/SteamImageEdit.java
@@ -28,60 +28,60 @@ public class SteamImageEdit extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        int autoWidth = 256;
-        int autoHeight = 256;
+        int autoWidth = 1280;
+        int autoHeight = 720;
 
         Group root = new Group();
 
-        Image image0 = new Image(SteamImageEdit.class.getResourceAsStream("con9.png"));
-        ImageView imageView0 = new ImageView(image0);
-        imageView0.setFitWidth(autoWidth);
-        imageView0.setFitHeight(autoHeight);
-
-//        Image image1 = new Image(SteamImageEdit.class.getResourceAsStream("knight_win.png"));
+//        Image image0 = new Image(SteamImageEdit.class.getResourceAsStream("Background.png"));
+//        ImageView imageView0 = new ImageView(image0);
+//        imageView0.setFitWidth(autoWidth);
+//        imageView0.setFitHeight(autoHeight);
+//
+//        Image image1 = new Image(SteamImageEdit.class.getResourceAsStream("characters.png"));
 //        ImageView imageView1 = new ImageView(image1);
-//        imageView1.setFitWidth(100);
-//        imageView1.setFitHeight(56);
-//
-//
-//
-//        Image image2 = new Image(SteamImageEdit.class.getResourceAsStream("platform.png"));
-//        ImageView imageView2 = new ImageView(image2);
-//        imageView2.setFitWidth(79);
-//        imageView2.setFitHeight(14);
-//
-//
-//        int lax = (autoWidth - 208) / 2;
-//        int lay = (autoHeight - 136) / 2;
-//        imageView1.setLayoutX(lax);
-//        imageView1.setLayoutY(lay);
+//        imageView1.setFitWidth(204);
+//        imageView1.setFitHeight(112);
+//        imageView1.setLayoutX((autoWidth - 408) / 2);
+//        imageView1.setLayoutY((autoHeight - 224) / 2);
+
+        Image image2 = new Image(SteamImageEdit.class.getResourceAsStream("platform.png"));
+        ImageView imageView2 = new ImageView(image2);
+//        imageView2.setFitWidth(158);
+//        imageView2.setFitHeight(29);
+//        int lax = (autoWidth - 316) / 2;
+//        int lay = (int) (imageView1.getLayoutY() + 212);
+//        imageView2.setLayoutX(lax);
+//        imageView2.setLayoutY(lay);
 
 
-        VBox vBox = new VBox();
-        vBox.setMinWidth(autoWidth);
-        vBox.setMinHeight(autoHeight);
-        vBox.setBackground(Background.fill(Color.BLACK));
-        vBox.setOpacity(0.6);
+//        VBox vBox = new VBox();
+//        vBox.setMinWidth(autoWidth);
+//        vBox.setMinHeight(autoHeight);
+//        vBox.setBackground(Background.fill(Color.BLACK));
+//        vBox.setOpacity(0.6);
 
 //
 //        imageView2.setLayoutX(imageView1.getLayoutX() + 12);
 //        imageView2.setLayoutY(imageView1.getLayoutY() + 56);
 
-//        Label label = new Label("SuperCowBox");
-//        label.setPrefWidth(autoWidth);
-//        label.setAlignment(Pos.CENTER);
-//        label.setFont(Font.font(64));
-//
-//        InnerShadow innerShadow = new InnerShadow();
-//        innerShadow.setOffsetX(4);
-//        innerShadow.setOffsetY(4);
-//        innerShadow.setChoke(0.2);
-//        innerShadow.setBlurType(BlurType.GAUSSIAN);
-//        innerShadow.setColor(Color.color(0.4, 0.5, 0.5));
-//        label.setEffect(innerShadow);
+        Label label = new Label("SuperCowBoy");
+        label.setPrefWidth(autoWidth);
+        label.setPrefHeight(autoHeight);
+        label.setAlignment(Pos.CENTER);
+        label.setFont(Font.font(64));
+        label.setLayoutY(20);
+
+        InnerShadow innerShadow = new InnerShadow();
+        innerShadow.setOffsetX(4);
+        innerShadow.setOffsetY(4);
+        innerShadow.setChoke(0.2);
+        innerShadow.setBlurType(BlurType.GAUSSIAN);
+        innerShadow.setColor(Color.color(0.4, 0.5, 0.5));
+        label.setEffect(innerShadow);
 
 
-        root.getChildren().addAll(imageView0,vBox);
+        root.getChildren().addAll(label);
         Scene scene = new Scene(root,autoWidth,autoHeight);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
